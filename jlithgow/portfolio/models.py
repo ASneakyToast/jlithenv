63dc93056365caebc92ff22150f4a19a32e02ca5
Joel Lithgow <jlithgow@Joels-MacBook-Pro.local>
2019-07-22 13:10:01 -0700

    First test of new models

--- jlithgow/portfolio/models.py
+++ jlithgow/portfolio/models.py
@@ -1,32 +1,109 @@
 from django.db import models
+import uuid
+
+#
+#   new data structure
+# Tags (Art, Design, Vault, Current)
+# Deck (Whole Project Series?) (everydays, thoughts, clusters)
+# Strips (An entirety of a piece (all photos))
+# Frame (a single part of a piece, a close up of a sculpture)
+#
+
+#     Abstracts     #
 
 
-
-class Section(models.Model):
-    section_title = models.CharField(max_length=250)
-    section_color = models.CharField(max_length=250, default='#656C6C')
-    section_description = models.CharField(max_length=250, default='No description provided.')
-
-    def __str__(self):
-        return self.section_title
+# class AbstractTags(models.Model):
+#     tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#
+#     class Meta:
+#         abstract = True
 
 
-class Project(models.Model):
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    model_title = models.CharField(max_length=250)
+#     Tags     #
+# ID = Unique identifier
+# Title = "Everydays", "Components"
+# Decks = all decks related
+# Strips = all strips related
+# Frames = All frames related
 
-    def __str__(self):
-        return self.model_title
+
+class Tag(models.Model):
+    tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tag_name = models.CharField(max_length=250)
 
 
-class Piece(models.Model):
-    piece_title = models.CharField(max_length=250)
-    date_created = models.DateTimeField('date created')
-    description_quick = models.CharField(max_length=250)
-    image_cover = models.ImageField(upload_to='images/pieces/', default='pic_folder/None/no-img.jpg')
+#     Deck Data     #
+# ID
+# Title
+# Tags
+# Strips
+# Frames
+# Time-Span = Start strip to end strip
 
-    def __str__(self):
-        return self.piece_title
+
+class Deck(models.Model):
+    deck_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    deck_title = models.CharField(max_length=250)
+    deck_tags = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+#     Strips Data     #
+# ID = Unique identifier
+# Title = "14_07_19", can only be one title
+# Alt-Text = Story/alt-text
+# Tags = Art, Current
+# Time-Span = start frame to end frame
+
+
+class Strip(models.Model):
+    strip_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    strip_title = models.CharField(max_length=250)
+    strip_altText = models.CharField(max_length=447)
+    strip_decks = models.ForeignKey(Deck, on_delete=models.CASCADE)
+
+
+#     Frame Data     #
+# ID = Unique identifier
+# Title = "Gallery_Shot", if same name = "Gallery_Shot--2"
+# Strips = Piece Name. "14_07_19"
+# Decks = Project Series "Everydays"
+# Tags = Art, Current
+# Time = Date: date made, time: time posted
+# Alt-Text Override =
+
+
+class Frame(models.Model):
+    frame_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    frame_title = models.CharField(max_length=250)
+    frame_strips = models.ForeignKey(Strip, on_delete=models.CASCADE)
+
+
+# Old Data things #
+#
+# class Section(models.Model):
+#     section_title = models.CharField(max_length=250)
+#     section_color = models.CharField(max_length=250, default='#656C6C')
+#     section_description = models.CharField(max_length=250, default='No description provided.')
+#
+#     def __str__(self):
+#         return self.section_title
+#
+#
+# class Project(models.Model):
+#     section = models.ForeignKey(Section, on_delete=models.CASCADE)
+#     model_title = models.CharField(max_length=250)
+#
+#     def __str__(self):
+#         return self.model_title
+#
+#
+# class Piece(models.Model):
+#     piece_title = models.CharField(max_length=250)
+#     date_created = models.DateTimeField('date created')
+#     description_quick = models.CharField(max_length=250)
+#     image_cover = models.ImageField(upload_to='images/pieces/', default='pic_folder/None/no-img.jpg')
+#
+#     def __str__(self):
+#         return self.piece_title
 
 # class Example(models.Model):
 #     author = Author.objects.get(name='Rod Dhl')
