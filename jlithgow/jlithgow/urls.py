509a1c7ebf023bd6d30cb7bbe5960d7e6013d6d2
Joel Lithgow <jlithgow@Joels-MacBook-Pro.local>
2019-08-02 18:55:45 -0700

    css changes, all to the Portoflio Index Page

--- jlithgow/jlithgow/urls.py
+++ jlithgow/jlithgow/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
+from . import views, settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
     path('portfolio/', include('portfolio.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
