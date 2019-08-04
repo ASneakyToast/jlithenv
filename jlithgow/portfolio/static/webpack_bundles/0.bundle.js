a0ac671f044e30626ff1637efa37cd11bf1d104b
Joel Lithgow <jlithgow@Joels-MacBook-Pro.local>
2019-08-04 11:50:12 -0700

    Fixed the webpack config to output a css file.

---
+++ jlithgow/portfolio/static/webpack_bundles/0.bundle.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./portfolio/static/portfolio/styles/app.scss":
+/*!****************************************************!*\
+  !*** ./portfolio/static/portfolio/styles/app.scss ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./portfolio/static/portfolio/styles/app.scss?");
+
+/***/ })
+
+}]);
--- jlithgow/webpack.config.js
+++ jlithgow/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+
     entry : "./portfolio/static/portfolio/styles/index.js",
     output: {
         path: path.resolve("./portfolio/static/webpack_bundles"),
@@ -9,17 +10,6 @@ module.exports = {
     },
 
     mode: 'development',
-
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        }]
-    },
 
     plugins:
         [new MiniCssExtractPlugin({
@@ -30,40 +20,25 @@ module.exports = {
         })
     ],
 
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                // fallback to style-loader in development
+                // 'style-loader',
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        publicPath: '../',
+                        hmr: process.env.NODE_ENV === 'development',
+                    },
+                },
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
+
 };
-//
-// let config = {
-//
-//     // entry: "./portfolio/static/portfolio/styles/index.js",
-//
-//     output: {
-//         path: path.resolve("./portfolio/static/webpack_bundles"),
-//         filename: "[name].bundle.js"
-//     },
-//
-//     mode: 'development',
-//
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.scss$/,
-//                 use: [
-//                     "style-loader", // creates style nodes from JS strings
-//                     "css-loader", // translates CSS into CommonJS
-//                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
-//                 ]
-//             }
-//         ]
-//     },
-//
-//     plugins: [
-//         new MiniCssExtractPlugin({
-//             // Options similar to the same options in webpackOptions.output
-//             // both options are optional
-//             filename: "[name].css",
-//             chunkFilename: "[id].css"
-//         })
-//     ],
-// };
-//
-// module.export = config;
